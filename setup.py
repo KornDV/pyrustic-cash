979e6dcd0233796f564e314a0f5feb9a2b1f8509
KornDV <korteam7@gmail.com>
2022-09-10 01:26:36 -0400

    Add rust install

--- setup.py
+++ setup.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env python
 from setuptools import dist
+
 dist.Distribution().fetch_build_eggs(['setuptools_rust'])
 
 from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(
     name="pyrustic-cash",
     version="0.1",
-    rust_extensions=[RustExtension(
-        ".pyrustic_cash.pyrustic_cash",
-        path="Cargo.toml", binding=Binding.PyO3)],
+    rust_extensions=[
+        RustExtension("pyrustic_cash.pyrustic_cash", path="Cargo.toml", binding=Binding.PyO3)
+    ],
     packages=["pyrustic_cash"],
     classifiers=[
-            "License :: OSI Approved :: MIT License",
-            "Development Status :: 3 - Alpha",
-            "Intended Audience :: Developers",
-            "Programming Language :: Python",
-            "Programming Language :: Rust",
-            "Operating System :: POSIX",
-            "Operating System :: MacOS :: MacOS X",
-        ],
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Rust",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+    ],
     zip_safe=False,
 )
