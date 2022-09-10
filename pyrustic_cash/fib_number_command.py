bb0b245b8af222e7fafbbec704b550b207407171
KornDV <korteam7@gmail.com>
2022-09-10 01:53:16 -0400

    Add rust install

---
+++ pyrustic_cash/fib_number_command.py
@@ -0,0 +1,7 @@
+import argparse
+from .pyrustic_cash import fibonacci_number
+def fib_number_command() -> None:
+    parser = argparse.ArgumentParser(description='Calculate Fibonacci numbers')
+    parser.add_argument('--number', action='store', type=int, required=True,help="Fibonacci number to becalculated")
+    args = parser.parse_args()
+    print(f"Your Fibonacci number is: "{fibonacci_number(n=args.number)}")
--- setup.py
+++ setup.py
@@ -22,5 +22,12 @@ setup(
         "Operating System :: POSIX",
         "Operating System :: MacOS :: MacOS X",
     ],
+    entry_points={
+        'console_scripts': [
+            'fib-number = pyrustic_cash.'
+            'fib_number_command:'
+            'fib_number_command',
+        ],
+    },
     zip_safe=False,
 )
